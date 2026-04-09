067ced2adb10de129618dc57f2854fa48d1b6010
bdchin88 <bdchin88@gmail.com>
2026-04-09 17:49:15 +0900

    =====

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // eslint 경고를 해결하기 위해 기존 eslint 설정을 제거하고 아래 속성만 남깁니다.
+  // 빌드 시 에러가 있어도 일단 완성을 시키도록 강제 설정
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  
-  // SWC 관련 경고 해결을 위한 최적화 (필요 시)
-  swcMinify: true,
 };
 
 export default nextConfig;
