6ee102297b5308849c93dc711373691c5d0dc68a
bdchin88 <bdchin88@gmail.com>
2026-04-09 17:11:37 +0900

    긑

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // 빌드 시 타입스크립트 에러가 있어도 무시하고 진행
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 빌드 시 ESLint(문법 검사) 에러가 있어도 무시하고 진행
-    ignoreDuringBuilds: true,
-  },
+  // eslint: { ... } 대신 이 방식을 권장하지만, 일단 무시해도 빌드는 됩니다.
 };
 
 export default nextConfig;
