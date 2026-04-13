c228a0236ac31d24517403a66b18682c3377cd56
bdchin88 <bdchin88@gmail.com>
2026-04-13 12:22:04 +0900

    260413

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+// @type {import('next').NextConfig} 
 const nextConfig = {
   // eslint 설정을 여기에 쓰지 마세요.
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+//  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
