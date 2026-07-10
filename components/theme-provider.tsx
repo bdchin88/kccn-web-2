15efe22db61eee375e55fef0e5c04cf5ea94b323
bdchin88 <bdchin88@gmail.com>
2026-07-10 13:59:54 +0900

    s1

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// React.ComponentProps를 사용하여 타입을 자동으로 가져옵니다.
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
