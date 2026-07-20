dc6a8b823ac69c7976f90374b642d349256022aa
bdchin88 <bdchin88@gmail.com>
2026-07-20 14:06:56 +0900

    start

--- next.config.mjs
+++ next.config.mjs
@@ -16,30 +16,42 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
+          // 1. HSTS 설정 (1년간 HTTPS 강제)
           {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
+          // 2. 클릭재킹 방지 (iframe 삽입 차단)
           {
             key: "X-Frame-Options",
             value: "DENY",
           },
+          // 3. MIME 타입 스니핑 방지
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
+          // 4. 리퍼러 정보 유출 제한
           {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
+          // 5. 브라우저 기능 제한 (카메라, 마이크 등 차단)
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
+            value: "camera=(), microphone=(), geolocation=()", // ◀ 지오로케이션(위치) 기능 차단
           },
+          // 6. XSS 필터링 활성화
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          // 7. 격리된 세션 환경을 제공하여 리소스 및 팝업 창 간의 보안 격리를 강화
           {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
+          // 7.
           {
             key: "Cross-Origin-Resource-Policy",
             value: "same-origin",
